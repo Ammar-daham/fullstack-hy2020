6f3555dc8ecb1075bb9068eb78e5f0e0e52852c2
Ammar Daham <ammar.daham@metropolia.fi>
2022-12-20 02:38:08 +0200

    Course information step8 done

--- part2/courseinfo/src/component/Course.js
+++ part2/courseinfo/src/component/Course.js
@@ -3,14 +3,22 @@ import Header from "./Header"
 import Total from "./Total"
 
 const Course = ({ course }) => {
-    console.log("Course: " , course)
     let sum = 0;
+    const total = course.parts.reduce((a, b) => {
+        return {
+            exercises: a.exercises + b.exercises
+        };
+    }, {
+        exercises: 0
+    });
+    
+    console.log(total);
     course.parts.map( part  => sum = sum + part.exercises)
     return (
         <>
             <Header course={course}/>
             <Content parts={course.parts}/>
-            <Total sum={sum}/>
+            <Total sum={total.exercises}/>
         </>
 
     )
