4f97c17a904f95354b39164411576e67e84fe2a1
Ammar Daham <ammar.daham@metropolia.fi>
2022-12-20 22:29:17 +0200

    The Phonebook Step4 done

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,17 +2,19 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    {
-      name: 'Arto Hellas',
-      number: '04022321',
-    },
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
   ])
 
   console.log(persons)
 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [search, setSearch] = useState([])
 
+  console.log('search: ', search)
   console.log('newName: ', newName)
 
   const addPerson = (event) => {
@@ -20,7 +22,7 @@ const App = () => {
     console.log('Name added: ', persons)
     const personObj = {
       name: newName,
-      number: newNumber
+      number: newNumber,
     }
     persons.filter((person) =>
       person.name === personObj.name
@@ -40,9 +42,27 @@ const App = () => {
     console.log('new number: ', event.target.value)
   }
 
+  const handleSearchInputChange = (event) => {
+    const found = persons.find(
+      (person) =>
+        person.name.toLowerCase() === event.target.value.toLowerCase(),
+    )
+    found
+      ? console.log(`${event.target.value} found`)
+      : console.log(`${event.target.value} not found`)
+
+    setSearch(found)
+    console.log('Found: ', search)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with{' '}
+        <input value={persons.name} onChange={handleSearchInputChange} />
+      </div>
+      <h2>Add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameInputChange} />
@@ -61,6 +81,17 @@ const App = () => {
             {person.name} {person.number}
           </p>
         ))}
+      </div>
+      <div>
+        <h2>Filtered person</h2>
+        {
+          search 
+          ?
+            <p key={search.name}>
+              {search.name} {search.number}
+            </p>
+          : <p>not found</p>
+        }
       </div>
     </div>
   )
