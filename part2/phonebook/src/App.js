4453f24e7297b2f73ee61837491e5452f92603bf
Ammar Daham <ammar.daham@metropolia.fi>
2022-12-28 23:20:32 +0200

    Phonebook step11 + step12 done

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,90 +4,88 @@ import PersonForm from './component/PersonForm'
 import Persons from './component/Persons'
 import Person from './component/Person'
 import personService from './services/person'
+import Notification from './component/Notification'
 
 const App = () => {
-
   const [persons, setPersons] = useState([])
-
+  const [successMessage, setSuccessMessage] = useState(null)
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filteredPerson, setFilteredPerson] = useState([])
 
   useEffect(() => {
-    personService.getAll()
-    .then(initialState => {
+    personService.getAll().then((initialState) => {
       console.log('Promise fulfilled')
       setPersons(initialState)
     })
   }, [])
 
-  console.log("persons: ", persons)
-
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-  const [filteredPerson, setFilteredPerson] = useState([])
-
-  console.log('Filtered Person: ', filteredPerson)
-  console.log('newName: ', newName)
-  console.log('newNumber: ', newNumber)
-
-  const addPerson = ( event )  => {
+  const addPerson = (event) => {
     event.preventDefault()
-    let personObj = persons.find(p => p.name === newName)
-    if(personObj) {
-      personObj = {...personObj, number: newNumber}
-      console.log("personU: ", personObj )
-      window.confirm(`${newName} is already added to phoneBook, replace the old number with a new one?`)
-      ? (
-        personService.update
-        (personObj.id , personObj)
-        .then(updatedPerson => {
-          setPersons(persons.map( 
-            person => person.name !== personObj.name ? person : updatedPerson))
-          setNewName('')
-          setNewNumber('')
-          console.log('updated person: ', updatedPerson)
-        })
+    let personObj = persons.find((p) => p.name === newName)
+    if (personObj) {
+      personObj = { ...personObj, number: newNumber }
+      window.confirm(
+        `${newName} is already added to phoneBook, replace the old number with a new one?`,
       )
-      : console.log('Old number not updated')
+        ? personService
+            .update(personObj.id, personObj)
+            .then((updatedPerson) => {
+              setPersons(
+                persons.map((person) =>
+                  person.name !== personObj.name ? person : updatedPerson,
+                ),
+              )
+              setNewName('')
+              setNewNumber('')
+              setSuccessMessage(`${updatedPerson.name}'s number changed`)
+              setTime(setSuccessMessage)
+            }).catch( () => {
+              setErrorMessage(`Information of ${personObj.name} has already been removed from server `)
+              setTime(setErrorMessage)
+            })
+        : console.log('Old number not updated')
     } else {
       personObj = {
         name: newName,
         number: newNumber,
       }
-      personService.create(personObj)
-      .then(returnedPerson => {
-        setPersons(persons.concat(returnedPerson))
-        setNewName('')
-        setNewNumber('')
-        console.log("response: ", returnedPerson)
-      })
+      personService
+        .create(personObj)
+        .then((returnedPerson) => {
+          setPersons(persons.concat(returnedPerson))
+          setNewName('')
+          setNewNumber('')
+          setSuccessMessage(`Added ${returnedPerson.name}`)
+          setTime(setSuccessMessage)
+        })
     }
-    
-
-    
   }
 
-  const handleNameInputChange = event => 
-    setNewName(event.target.value)
+  const setTime = (message) => {
+    setTimeout(() => {
+      message(null)
+    }, 5000)
+  }
 
-  const handleNumberInputChange = event => 
-    setNewNumber(event.target.value)
-    
+  const handleNameInputChange = (event) => setNewName(event.target.value)
+
+  const handleNumberInputChange = (event) => setNewNumber(event.target.value)
 
   const handleSearchInputChange = (event) => {
     const found = persons.find(
       (person) =>
         person.name.toLowerCase() === event.target.value.toLowerCase(),
     )
-    found
-      ? console.log(`${event.target.value} found`)
-      : console.log(`${event.target.value} not found`)
-
     setFilteredPerson(found)
-    console.log('Found: ', found)
   }
 
   return (
     <div>
       <h2>PhoneBook</h2>
+
+      <Notification successMessage={successMessage} errorMessage={errorMessage} />
 
       <Filter name={persons.name} handleChange={handleSearchInputChange} />
 
@@ -103,23 +101,27 @@ const App = () => {
 
       <h3>Numbers</h3>
 
-    {
-      persons.map(person => (
-        <Persons key={person.id} person={person} handleDelete={() => {
-          window.confirm(`Delete ${person.name}`)
-          ?
-            personService.deletePerson(person.id)
-            .then( () => 
-              personService.getAll()
-              .then(initialState => {
-                console.log('Promise fulfilled')
-                setPersons(initialState)
-              })
-            )
-          :console.log('not deleted')
-        }} />
-        )) 
-    }
+      {persons.map((person) => (
+        <Persons
+          key={person.id}
+          person={person}
+          handleDelete={() => {
+            window.confirm(`Delete ${person.name}`)
+              ? personService.deletePerson(person.id).then(() => {
+                  setSuccessMessage(`Deleted ${person.name}`)
+                  setTime(setSuccessMessage)
+                  personService.getAll().then((initialState) => {
+                    console.log('Promise fulfilled')
+                    setPersons(initialState)
+                  })
+                }).catch( () => {
+                  setErrorMessage(`${person.name} has already been removed from the server`)
+                  setTime(setErrorMessage)
+                })
+              : console.log('not deleted')
+          }}
+        />
+      ))}
 
       <h3>Filtered person</h3>
 
---
+++ part2/phonebook/src/component/Notification.js
@@ -0,0 +1,22 @@
+import '../index.css'
+
+const Notification = ({ successMessage, errorMessage }) => {
+  if (successMessage === null && errorMessage === null) {
+    return null
+   }
+
+  return (
+    <div>
+        {
+            successMessage &&
+            <div className="success">{successMessage}</div>
+        }
+        {
+            errorMessage &&
+            <div className="error">{errorMessage}</div>
+        }
+    </div>
+  )
+}
+
+export default Notification
