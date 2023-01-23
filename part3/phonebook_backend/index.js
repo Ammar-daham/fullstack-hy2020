0ea76c7813bcb33031485d9411b59ddd2a6a4a4f
Ammar Daham <ammar.daham@metropolia.fi>
2023-01-23 12:23:35 +0200

    Phonebook backend step2 done

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -2,37 +2,43 @@ const express = require('express')
 const app = express()
 
 const persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
+  {
+    id: 1,
+    name: 'Arto Hellas',
+    number: '040-123456',
+  },
+  {
+    id: 2,
+    name: 'Ada Lovelace',
+    number: '39-44-5323523',
+  },
+  {
+    id: 3,
+    name: 'Dan Abramov',
+    number: '12-43-234345',
+  },
+  {
+    id: 4,
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122',
+  },
 ]
 
+const date = new Date()
+
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World</h1>')
+  res.send('<h1>Hello World</h1>')
+})
+
+app.get('/info', (req, res) => {
+  res.send(`<p>PhoneBook has info for ${persons.length} people</p>
+     <p>${date}</p>`)
 })
 
 app.get('/api/persons', (req, res) => {
-    console.log(persons)
-    res.json(persons)
+  console.log(persons)
+  res.json(persons)
 })
-
 
 const PORT = 3001
 app.listen(PORT)
