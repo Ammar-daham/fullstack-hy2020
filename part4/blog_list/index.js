9fc716184895489518c8a091d6561ccc0da330b2
Ammar Daham <ammar.daham@metropolia.fi>
2023-02-07 17:59:27 +0200

    config.js and logger.js init

--- part4/blog_list/index.js
+++ part4/blog_list/index.js
@@ -3,6 +3,8 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
 const blogSchema = new mongoose.Schema({
   title: String,
@@ -16,7 +18,7 @@ const Blog = mongoose.model('Blog', blogSchema)
 mongoose.set('strictQuery', false)
 
 
-const mongoUrl = process.env.MONGODB_URI
+const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl)
 
 app.use(cors())
@@ -40,7 +42,7 @@ app.post('/api/blogs', (request, response) => {
     })
 })
 
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
