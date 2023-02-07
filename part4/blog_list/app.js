e3a2fc9ec996eaefb66d33943f7782bc55749565
Ammar Daham <ammar.daham@metropolia.fi>
2023-02-07 18:43:58 +0200

    blog-list step2 done

--- part4/blog_list/app.js
+++ part4/blog_list/app.js
@@ -3,6 +3,7 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const blogRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 
@@ -10,21 +11,22 @@ mongoose.set('strictQuery', false)
 
 logger.info('connecting to', config.MONGODB_URI)
 
-
-const mongoUrl = config.MONGODB_URI
-mongoose.connect(config.MONGODB_URI)
-    .then(() => {
-        logger.info('connected to MongoDB')
-    })
-    .catch((error) => {
-        logger.error('error connecting to MongoDB: ', error.message)
-    })
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB: ', error.message)
+  })
 
 app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
 
-
+app.use(middleware.requestLogger)
 app.use('/api/blogs/', blogRouter)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
