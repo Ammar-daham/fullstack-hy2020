72ea6bd1330631928ec80fc6501f1511db0a1a47
Ammar Daham <ammar.daham@metropolia.fi>
2023-02-19 22:15:53 +0200

    bloglist end to end testing, step1 done

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,5 +1,5 @@
 
-describe('template spec', () => {
+describe('Blog app', () => {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
     cy.visit('http://localhost:3000')
