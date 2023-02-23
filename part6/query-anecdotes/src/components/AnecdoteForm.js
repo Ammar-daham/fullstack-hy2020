ed982af2dc1cea0e7076ac095ca7dbba635f0cee
Ammar Daham <ammar.daham@metropolia.fi>
2023-02-23 18:51:39 +0200

    Exercise 6.21 done

--- part6/query-anecdotes/src/components/AnecdoteForm.js
+++ part6/query-anecdotes/src/components/AnecdoteForm.js
@@ -1,10 +1,22 @@
+import { useMutation, useQueryClient } from "react-query"
+import { createAnecdote } from "../request"
+
 const AnecdoteForm = () => {
+
+  const queryClient = useQueryClient()
+  const newAnecdoteMutation = useMutation(createAnecdote, {
+    onSuccess: () => {
+      queryClient.invalidateQueries('anecdotes')
+    }
+  })
 
   const onCreate = (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
-    console.log('new anecdote')
+    content.length >= 5
+    ? newAnecdoteMutation.mutate({content, votes: 0})
+    : console.log('content should be at least 5 char')
 }
 
   return (
